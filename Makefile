b91cbfea485a5bc1b2ff1d566bbe721204299c57
Maxx Gentry <lilmaxx4@gmail.com>
2025-08-19 01:56:26 +0000

    initial portaudio tests

---
+++ Makefile
@@ -0,0 +1,14 @@
+exec:
+	go build -o go-dsp && ./go-dsp
+
+dockerc:
+	docker build -t go-dsp-image .
+	docker stop go-dsp
+	docker rm go-dsp
+	docker container create -p 8080:8080 --name go-dsp go-dsp-image
+
+dockerup:
+	docker start go-dsp
+
+dockerdown:
+	docker stop go-dsp
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
-package godsp
+package main
 
-func main() error {
-	return nil
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gordonklaus/portaudio"
+)
+
+func main() {
+
+	if portaudio.Initialize() != nil {
+		panic(errors.New("Portaudio couldn't initialize"))
+	}
+
+	defer portaudio.Terminate()
+	devices, err := portaudio.Devices()
+
+	if err != nil {
+		panic(errors.New("Error enumerating audio devices"))
+	}
+
+	for _, d := range devices {
+		fmt.Println(d.Name)
+	}
 }
