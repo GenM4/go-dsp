7ab43e32735e7abdd5b04fdafdc314bd3f3db285
Maxx Gentry <lilmaxx4@gmail.com>
2025-08-20 04:06:42 +0000

    testing sine gen

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	p := buildStreamParams(nil, devices[1], 2)
 
-	sine, err := newStereoSine(256, 320, p)
+	sine, err := newStereoSine(1000, 1000, p)
 	if err != nil {
 		panic(err)
 	}
