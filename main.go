2d7f0d72d6dc76dd41953390994ef5e79205a949
Maxx Gentry <lilmaxx4@gmail.com>
2025-08-20 04:15:29 +0000

    testing sine gen

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 	"github.com/gordonklaus/portaudio"
 )
 
-const sampleRate = 48000
-
 func main() {
 
 	if portaudio.Initialize() != nil {
@@ -69,7 +67,7 @@ func (g *stereoSine) processAudio(out [][]float32) {
 }
 
 func newStereoSine(freqL, freqR float64, p *portaudio.StreamParameters) (*stereoSine, error) {
-	s := &stereoSine{nil, freqL / sampleRate, 0, freqR / sampleRate, 0}
+	s := &stereoSine{nil, freqL / p.SampleRate, 0, freqR / p.SampleRate, 0}
 
 	var err error
 	s.Stream, err = portaudio.OpenStream(*p, s.processAudio)
